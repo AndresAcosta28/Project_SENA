62cb87879a1d6ac58b96e2cc017d66018727e76c
AndresAcosta28 <andresito20an@hotmail.com>
2025-11-17 16:07:28 -0500

    Arreglando Boton backend

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,14 +1,14 @@
-async function llamarAPI() {
+document.getElementById("check-backend").addEventListener("click", async () => {
     const url = "http://backend-env-6e3f16d0.eba-prjwqm57.us-east-1.elasticbeanstalk.com";
 
     try {
         const response = await fetch(url);
         const data = await response.json();
-        document.getElementById("respuesta").innerText =
-            "Respuesta del backend: " + data.message;
 
+        document.getElementById("backend-response").innerText =
+            "Respuesta del backend: " + data.message;
     } catch (error) {
-        document.getElementById("respuesta").innerText =
+        document.getElementById("backend-response").innerText =
             "Error al contactar el backend ❌ " + error;
     }
-}
+});
