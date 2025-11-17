679410d61b81fb9f87931ece140a1a08215b63ad
AndresAcosta28 <andresito20an@hotmail.com>
2025-11-16 23:29:38 -0500

    Ajuste despliegue

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -1,37 +1,24 @@
-# --- Outputs útiles ---
-
-output "frontend_bucket_name" {
-  description = "Nombre del bucket S3 del frontend"
-  value       = aws_s3_bucket.frontend_bucket.id
-}
-
-output "frontend_bucket_website_endpoint" {
-  description = "Endpoint del website S3"
-  value       = aws_s3_bucket_website_configuration.frontend_website.website_endpoint
-}
-
-output "cloudfront_domain" {
-  description = "Dominio de CloudFront para acceder al frontend"
-  value       = aws_cloudfront_distribution.frontend_cdn.domain_name
-}
-
-output "cloudfront_url" {
-  description = "URL completa de CloudFront"
+output "frontend_cloudfront_url" {
+  description = "URL de CloudFront para acceder al frontend"
   value       = "https://${aws_cloudfront_distribution.frontend_cdn.domain_name}"
 }
 
-output "rds_endpoint" {
-  description = "Endpoint de la base de datos RDS"
-  value       = aws_db_instance.rds_mysql.endpoint
-  sensitive   = true
-}
-
-output "rds_database_name" {
-  description = "Nombre de la base de datos"
-  value       = aws_db_instance.rds_mysql.db_name
+output "frontend_s3_bucket" {
+  description = "Nombre del bucket S3 para deploy del frontend"
+  value       = aws_s3_bucket.frontend_bucket.id
 }
 
 output "backend_url" {
-  description = "URL del backend en Elastic Beanstalk"
-  value       = "http://${aws_elastic_beanstalk_environment.backend_env.cname}"
+  description = "URL de Elastic Beanstalk para el backend API"
+  value       = "http://${aws_elastic_beanstalk_environment.backend_env.endpoint_url}"
+}
+
+output "database_endpoint" {
+  description = "Endpoint de RDS MySQL"
+  value       = aws_db_instance.rds_mysql.address
+}
+
+output "database_port" {
+  description = "Puerto de RDS MySQL"
+  value       = aws_db_instance.rds_mysql.port
 }
