ff7d7e0e398daab0a519c7e0568940a3a3a04e7a
AndresAcosta28 <andresito20an@hotmail.com>
2025-11-17 19:30:59 -0500

    Reversionado

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -10,8 +10,30 @@ variable "db_username" {
   default     = "admin"
 }
 
+variable "db_password" {
+  description = "Contraseña de la base de datos"
+  type        = string
+  sensitive   = true
+}
+
 variable "environment" {
   description = "Nombre del entorno (dev, prod, etc.)"
   type        = string
   default     = "dev"
+}
+
+variable "amplify_repo_url" {
+  description = "URL del repositorio para Amplify"
+  type        = string
+}
+
+variable "amplify_branch_name" {
+  description = "Nombre de la rama para despliegue"
+  type        = string
+}
+
+variable "github_token" {
+  description = "Token OAuth para conectar GitHub con Amplify"
+  type        = string
+  sensitive   = true
 }
